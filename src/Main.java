0000526a0e5682686ca2feb879006f31634a065a
yerkemakhanbet <erkebylan98@gmail.com>
2023-09-04 21:02:23 +0600

    changed Main class again

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String git = "Hello, GitHub!";
         System.out.println(git);
+        System.out.println(git.toUpperCase());
     }
 }
